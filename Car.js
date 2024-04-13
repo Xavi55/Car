df2bb58e1a1d0b0d59c4dee23b9aa845cd5eef46
xavi55 <evinxavier5@gmail.com>
2024-04-13 01:48:01 -0400

    newish car

---
+++ Car.js
@@ -0,0 +1,13 @@
+//some id
+module.exports = {
+
+    Make: 'Mazda',
+    Model: 'Mazdaspeed 3',
+    Year: 2010,
+    
+    Plate: "D88RJT",
+    State: "NJ",
+    //contacts
+    email: 'realxav8@gmail.com',
+  };
+  
